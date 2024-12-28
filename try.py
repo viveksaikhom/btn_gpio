f22cbf4012da502363846957f1186c1b9edecd28
viveksaikhom <viveksaikhom@gmail.com>
2024-12-28 19:23:15 +0530

    m

--- try.py
+++ try.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 user = input("Pin number: ")
 BUTTON_PIN = user
 
-GPIO.setup(BUTTON_PIN, GPIO.IN)
+GPIO.setup(BUTTON_PIN, GPIO.OUT)
 
 try:
     print(f"Initial state of pin {BUTTON_PIN}: {'HIGH' if GPIO.input(BUTTON_PIN) else 'LOW'}")
