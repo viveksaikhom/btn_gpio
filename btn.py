5aba55616228c1defd1112b317b5db248f57b7b0
viveksaikhom <viveksaikhom@gmail.com>
2024-12-28 18:39:18 +0530

    4th

--- btn.py
+++ btn.py
@@ -5,19 +5,19 @@ GPIO.setmode(GPIO.BOARD)
 
 BUTTON_PIN = 13
 
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_PIN, GPIO.IN)
 
 print("Waiting for button press...")
 
 try:
     while True:
         if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-            print("Button Pressed! Exiting program.")
+            print("Button Pressed!")
             break
         time.sleep(0.3)
 
 except KeyboardInterrupt:
-    print("\nProgram interrupted manually.")
+    print("\nExiting program.")
 
 finally:
     GPIO.cleanup()
